a1df4ae30091de1274dbbe6ae2e4b4907375f9aa
Gerald Wodni <gerald.wodni@gmail.com>
2018-12-24 00:55:56 +0100

    only PWM1+2 see to be working, broken attempt

--- src/pwm.fs
+++ src/pwm.fs
@@ -10,24 +10,40 @@ compiletoflash
     init-i2c
 
     pwm1 gpio-alternate
+    1 GPIOA gpio-alternate
+    2 GPIOA gpio-alternate
+    3 GPIOA gpio-alternate
+    pwm2 gpio-alternate
 
     $01 RCC_APB1ENR bis!    \ enable timer 2 clock
 
+    $80 TIM2 TIMx_CR1 hbis! \ enable timer 2
     72 TIM2 TIMx_PSC h!     \ prescaler (72MHz ^= 1us)
 
     20000 TIM2 TIMx_ARR  h! \ top register (20ms)
     1000  TIM2 TIMx_CCR1 h! \ duty cycle (1ms)
+    1000  TIM2 TIMx_CCR3 h! \ duty cycle (1ms)
 
-    $68 TIM2 TIMx_CCMR1_Output h!    \ PWM mode
-    $01 TIM2 TIMx_CCER h!   \ OC1 signal = output
+    $68 TIM2 TIMx_CCMR1_Output h!    \ PWM mode CH1
+    $68 TIM2 TIMx_CCMR2_Output h!    \ PWM mode CH3
+    $99 TIM2 TIMx_CCER h!   \ OC1 & OC3 signal = output
+
+    1000  TIM2 TIMx_CCR2 h! \ duty cycle (1ms)
+    $6800 TIM2 TIMx_CCMR1_Output hbis!    \ PWM mode CH2
+    $6800 TIM2 TIMx_CCMR2_Output hbis!    \ PWM mode CH4
 
     $01 TIM2 TIMx_EGR hbis! \ update shadow registers
-    $81 TIM2 TIMx_CR1 hbis! \ enable timer 2
+    $01 TIM2 TIMx_CR1 hbis! \ enable timer 2
 ;
 
 \ set to 0-2000
 : pwm1! ( n -- )
     2000 min 0 max 500 +
     TIM2 TIMx_CCR1 h! ;
+
+\ set to 0-2000
+: pwm2! ( n -- )
+    2000 min 0 max 500 +
+    TIM2 TIMx_CCR3 h! ;
 
 init-pwm
