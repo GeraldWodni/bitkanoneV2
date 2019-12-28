96b684b1533487fa6749a52d1c46c085af20a92f
Gerald Wodni <gerald.wodni@gmail.com>
2019-12-28 12:22:27 +0100

    turtle: bresenham ported from wave(t800 nokia 6610 display) - not yet working

--- src/turtle.fs
+++ src/turtle.fs
@@ -7,6 +7,9 @@
 0 variable ty
 $000707 variable tcolor
 
+: signof ( n2 -- n2 )
+    0< if -1 else 1 then ;
+
 \ move turtle
 : goto ( x y -- )
     ty ! tx ! ;
@@ -37,38 +40,60 @@ $000707 variable tcolor
 \         y = y + 1
 \     Pixel (x, y) einfärben
 
-0 variable (line)-raise
+0 variable sx
+0 variable sy
+0 variable (liner)
 
-\ bresenham line code (only for 1st quadrant, and dx >= dy)
-: (line) ( x y -- )
-    over >r \ save x
-    ty @ - swap tx @ - ( dy dx )
-    over   2* -rot ( do dy dx )
-    2dup - 2* -rot ( do dno dy dx )
-    swap 2* swap - ( do dno d )
+\ line in 1st octant
+: (line-shallow) ( dx dy err1 -- dx dy err2 )
+    over - \ dx dy err-dy
+    dup 0< if
+        3 pick + \ dx dy err-dy+dx
+        sx @ tx +!
+        sy @ ty +!
+    else
+        sx @ tx +!
+    then ;
 
-    (point) \ draw starting point
-
-    begin
-        1 tx +!         \ increment x
-        dup 0<= if      \ if d <= 0 remain
-            2 pick +    \ d = d + do
-        else            \ otherwise raise
-            over +      \ d = d + dno
-            (line)-raise @ execute  \ perform y++ or alike
-        then
-
-        (point)         \ draw new point
-    r@ tx @ = until
-
-    drop 2drop          \ clean do dno and d
-    r> drop ;           \ clear xe
-
-: ty1+ ( -- )
-    1 ty +! ;
+\ line in 2nd octant
+: (line-steep) ( dx dy err1 -- dx dy err2 )
+    3 pick - \ dx dy err-dx
+    dup 0< if
+        over + \ dx dy err-dx+dy
+        sy @ ty +!
+        sx @ tx +!
+    else
+        sy @ ty +!
+    then ;
 
 : line ( x y -- )
-    ['] ty1+ (line)-raise !
-    (line) ;
+    over tx @ - \ x y dx
+    over ty @ - \ x y dx dy
+    over signof sx !
+    dup  signof sy !
+    abs swap abs swap \ x y |dx| |dy|
 
-$000707 tcolor !
+    2dup > if   \ dx > dy
+        ['] (line-shallow) (liner) !
+        over \ shwallowcount = dx
+        dup 2/ \ err = dx/2
+    else
+        ['] (line-steep) (liner) !
+        dup  \ shwallowcount = dy
+        dup 2/ \ err = dy/2
+    then
+    swap \ change err and shwallowcount
+
+    (point) \ startpoint
+    \ dx dy err shwallowcount
+    0 do
+        (liner) @ execute
+        (point)
+    loop
+    drop 2drop 2drop ;
+
+: line+ ( dx dy -- )
+    ty + swap
+    tx + swap
+    line ;
+
